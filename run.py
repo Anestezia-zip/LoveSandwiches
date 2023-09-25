a795eba3311d511b8a546a8578c7fc8899cde21f
Anestezia-zip <plekatybtc@gmail.com>
2023-09-25 20:37:12 +0100

    resolved bug: deleted parentheses from var worksheet

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ def update_worksheet(data, worksheet):
     Update the relevant worksheet with the data provided 
     """
     print(f"Updating {worksheet} worksheet...\n")
-    worksheet_to_update = SHEET.worksheet('worksheet')
+    worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
 
